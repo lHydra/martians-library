a41cba01d33b72ae847cb6cfbb6d3b43c9ae0124
Georgiy <g3orge.miller@gmail.com>
2019-06-06 19:45:11 +0300

    basic set up frontend

---
+++ app/javascript/components/Provider/index.js
@@ -0,0 +1,11 @@
+// app/javascript/components/Provider/index.js
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { createCache, createClient } from '../../utils/apollo';
+
+export default ({ children }) => (
+  <ApolloProvider client={createClient(createCache())}>
+    {children}
+  </ApolloProvider>
+);
+
